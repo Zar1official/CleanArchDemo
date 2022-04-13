21e86d55567f06ed5269b6e69a17c0525e86966b
Zar <dimzar233445@gmail.com>
2022-04-13 22:57:57 +0300

    moved to hilt

--- app/src/main/java/ru/zar1official/cleanarchdemo/ui/screens/list/CharactersListViewModel.kt
+++ app/src/main/java/ru/zar1official/cleanarchdemo/ui/screens/list/CharactersListViewModel.kt
@@ -3,10 +3,10 @@ package ru.zar1official.cleanarchdemo.ui.screens.list
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 import ru.zar1official.cleanarchdemo.domain.models.Character
 import ru.zar1official.cleanarchdemo.domain.usecases.GetAllCharactersUseCase
@@ -16,11 +16,10 @@ import javax.inject.Inject
 class CharactersListViewModel @Inject constructor(private val getAllCharactersUseCase: GetAllCharactersUseCase) :
     ViewModel() {
     val characters: Flow<CharactersState> = flow {
-        delay(2000L)
         emit(CharactersState.Loading)
         val result = getAllCharactersUseCase.invoke()
         emit(result)
-    }.flowOn(Dispatchers.IO)
+    }
 
     private val _eventFlow = MutableSharedFlow<CharacterListEvent>()
     val eventFlow: SharedFlow<CharacterListEvent> = _eventFlow
